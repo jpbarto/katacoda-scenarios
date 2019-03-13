bad44abfba5fa29657ef4090b77f978266e1d880
Jason Barto <jason.p.barto@gmail.com>
2019-03-13 01:10:40 +0000

    Updated

--- mxnet-gluon-crash_course/initCourse.sh
+++ mxnet-gluon-crash_course/initCourse.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-echo #######################################################
-echo ## PLEASE WAIT WHILE WE DOWNLOAD THE MXNET CONTAINER ##
-echo #######################################################
-echo ...
 docker pull mxnet/python:latest_cpu_mkl_py3
--- mxnet-gluon-crash_course/startCourse.sh
+++ mxnet-gluon-crash_course/startCourse.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+echo #######################################################
+echo ## PLEASE WAIT WHILE WE DOWNLOAD THE MXNET CONTAINER ##
+echo #######################################################
+echo ...
 docker run -ti --rm mxnet/python:latest_cpu_mkl_py3 python3
